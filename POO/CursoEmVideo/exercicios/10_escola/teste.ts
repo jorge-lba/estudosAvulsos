41bc7b0d2f6011c66e33356b41ecbbe064a44650
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-07-14 22:58:38 -0300

    Add - Teste para a classe Professor

--- POO/CursoEmVideo/exercicios/10_escola/teste.ts
+++ POO/CursoEmVideo/exercicios/10_escola/teste.ts
@@ -1,5 +1,8 @@
 import Pessoa from './pessoa.classe'
 import Aluno from './aluno.classe'
+import Professor from './professor.classe'
+
+console.log('\nPessoa ------------------------ \n')
 
 const pessoa = new Pessoa(
   'Jorge',
@@ -9,7 +12,22 @@ const pessoa = new Pessoa(
 
 pessoa.fazerAniversario()
 
+console.log('\nAluno ------------------------ \n')
+
 const aluno = new Aluno('Jorge', 28, 'Masculino')
 
 aluno.matricular('informática')
+
+console.log('\n------------------------ \n')
+
 aluno.cancelarMatricula()
+
+console.log('\nProfessor ------------------------ \n')
+
+const professor = new Professor('Marcos', 38, 'Masculino')
+
+professor.receberAumento(25.28983)
+professor.especialista = 'Programação'
+
+console.log(professor.nome)
+console.log(professor.especialista)
