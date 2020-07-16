ac80477eb68eb4a3048ee354f89089c8dbd39283
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-07-16 20:27:29 -0300

    Add - Classe Abstrata Animal

---
+++ POO/CursoEmVideo/exercicios/12_sobrecarga/animal.abstrata.ts
@@ -0,0 +1,36 @@
+export default abstract class Animal {
+  constructor(
+    private _peso:number,
+    private _idade:number,
+    private _membros:number
+  ){}
+
+  set peso(peso:number){
+    this._peso = peso
+  }
+
+  set idade(idade:number){
+    this._idade = idade
+  }
+
+  set membros(membros:number){
+    this._membros = membros
+  }
+
+  get peso(){
+    return this._peso
+  }
+
+  get idade(){
+    return this._idade
+  }
+
+  get membro(){
+    return this._membros
+  }
+
+  abstract locomover():void
+  abstract alimentar():void
+  abstract emitirSom():void
+
+}
