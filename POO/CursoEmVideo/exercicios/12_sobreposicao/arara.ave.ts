eb0e91b5fa022dbba461f5a194367b24289762ee
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-07-17 00:08:54 -0300

    Add - Classes GoldFish e Arara

---
+++ POO/CursoEmVideo/exercicios/12_sobreposicao/arara.ave.ts
@@ -0,0 +1,7 @@
+import Ave from "./ave.classe";
+
+export default class Arara extends Ave {
+  constructor(peso:number, idade:number, membros:number){
+    super(peso, idade, membros)
+  }
+}
---
+++ POO/CursoEmVideo/exercicios/12_sobreposicao/goldFish.peixe.ts
@@ -0,0 +1,7 @@
+import Peixe from "./peixe.classe";
+
+export default class GoldFish extends Peixe{
+  constructor(peso:number, idade:number, membros:number){
+    super(peso, idade, membros)
+  }
+}
