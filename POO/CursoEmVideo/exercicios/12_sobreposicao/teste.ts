2372964c7bf0fe1833ca5de26bc0972a7c082b6e
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-07-16 23:29:31 -0300

    Add - Teste para a Classe Reptil

--- POO/CursoEmVideo/exercicios/12_sobreposicao/teste.ts
+++ POO/CursoEmVideo/exercicios/12_sobreposicao/teste.ts
@@ -1,8 +1,9 @@
 import Mamifero from './mamifero.classe'
-
-const mamifero = new Mamifero(14, 4, 4)
+import Reptil from './reptil.classe'
 
 console.log('\nMamífero -------\n')
+
+const mamifero = new Mamifero(14, 4, 4)
 
 mamifero.corDoPelo = 'Preto'
 
@@ -14,3 +15,18 @@ console.log(mamifero.membros)
 mamifero.alimentar()
 mamifero.emitirSom()
 mamifero.locomover()
+
+console.log('\nRéptil -------\n')
+
+const reptil = new Reptil(.250, 1, 4)
+
+reptil.corDaEscama = 'Verde'
+
+console.log(reptil.corDaEscama)
+console.log(reptil.peso)
+console.log(reptil.idade)
+console.log(reptil.membros)
+
+reptil.alimentar()
+reptil.emitirSom()
+reptil.locomover()
