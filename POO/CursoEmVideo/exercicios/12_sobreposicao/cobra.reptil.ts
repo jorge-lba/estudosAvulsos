d2e5ee42efc8298d6fa7d66104d6fabe44c6e59a
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-07-17 00:00:33 -0300

    Add - Classe Cobra

---
+++ POO/CursoEmVideo/exercicios/12_sobreposicao/cobra.reptil.ts
@@ -0,0 +1,7 @@
+import Reptil from "./reptil.classe";
+
+export default class Cobra extends Reptil {
+  constructor(peso:number, idade:number, membros:number){
+    super(peso, idade, membros)
+  }
+}
